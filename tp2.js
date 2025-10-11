33bc4c482edccc9425aea10f5e96e9301d62a614
Bog154 <luciostefano2004@gmail.com>
2025-10-11 14:18:32 -0300

    modified:   assets/Cinematica.txt 	new file:   assets/buscando.gif 	renamed:    assets/mansion01.jpg -> assets/mansion0.jpg 	deleted:    assets/mansion00.jpg 	deleted:    assets/mansion07.jpg 	new file:   assets/mansion1.jpg 	deleted:    assets/mansion10-0000.jpg 	deleted:    assets/mansion10-0001.jpg 	deleted:    assets/mansion10-0002.jpg 	deleted:    assets/mansion10-0003.jpg 	deleted:    assets/mansion10-0004.jpg 	deleted:    assets/mansion10-0005.jpg 	deleted:    assets/mansion10-0006.jpg 	deleted:    assets/mansion10-0007.jpg 	deleted:    assets/mansion10-0008.jpg 	deleted:    assets/mansion10-0009.jpg 	deleted:    assets/mansion10-0010.jpg 	deleted:    assets/mansion10-0011.jpg 	deleted:    assets/mansion10-0012.jpg 	deleted:    assets/mansion10-0013.jpg 	deleted:    assets/mansion10-0014.jpg 	deleted:    assets/mansion10-0015.jpg 	deleted:    assets/mansion10-0016.jpg 	deleted:    assets/mansion10-0017.jpg 	deleted:    assets/mansion10-0018.jpg 	deleted:    assets/mansion10-0019.jpg 	deleted:    assets/mansion10-0020.jpg 	new file:   assets/mansion10.gif 	renamed:    assets/mansion06.jpg -> assets/mansion2.jpg 	renamed:    assets/mansion02.jpg -> assets/mansion3.jpg 	renamed:    assets/mansion03.jpg -> assets/mansion4.jpg 	new file:   assets/mansion5.jpg 	new file:   assets/pandillaAuto.jpg 	deleted:    assets/pandillaAuto.png 	renamed:    assets/anim00.jpg -> assets/pantalla0.jpg 	renamed:    assets/anim01.jpg -> assets/pantalla1.jpg 	renamed:    assets/mansion11.jpg -> assets/pantalla10.jpg 	new file:   assets/pantalla11.jpg 	renamed:    assets/anim02.jpg -> assets/pantalla2.jpg 	renamed:    assets/anim03.jpg -> assets/pantalla3.jpg 	renamed:    assets/anim04.jpg -> assets/pantalla4.jpg 	renamed:    assets/anim05.jpg -> assets/pantalla5.jpg 	renamed:    assets/anim06.jpg -> assets/pantalla6.jpg 	new file:   assets/pantalla7.jpg 	new file:   assets/pantalla8.jpg 	new file:   assets/pantalla9.jpg 	new file:   assets/scooby.png 	modified:   tp2.js

--- tp2.js
+++ tp2.js
@@ -1,164 +1,518 @@
 let pag;
+let gif = [];
 let fondo = [];
-let fuente
+let fuente;
 let dialogos;
 let animacion;
-let puerta, llave, libro, velas;
 let color1, color2;
+let segundos, marcaTiempo, contadorIndice;
 
-function preload(){
+// Botones globales
+let boton1, boton2, boton3, botonSig;
+let botonInicio, botonCreditos;
+let botonQuedarse, botonIrse;
+let boton_buscarotraentrada, boton_intentarabrirpuerta, boton_buscarenmaquinademisterio;
 
+function preload() {
   fuente = loadFont('assets/Creepster-Regular.ttf');
   dialogos = loadStrings('assets/Cinematica.txt');
-  fondo.push( loadImage('assets/inicio.png'));
-  for(i = 0; i < 7; i++){
-    fondo.push(loadImage('assets/anim0' + i + '.jpg'));
+  fondo.push(loadImage('assets/inicio.png'));
+  for (i = 0; i < 12; i++) {
+    fondo.push(loadImage('assets/pantalla' + i + '.jpg'));
   }
-  for(i = 0; i < 4; i++){
-    fondo.push(loadImage('assets/mansion0' + i + '.jpg'));
+  for(i = 0; i < 6; i++){
+    fondo.push(loadImage('assets/mansion'+ i +'.jpg'));
   }
+  gif.push(loadImage('assets/buscando.gif'));
+  gif.push(loadImage('assets/mansion10.gif'));
 }
 
 function setup() {
   createCanvas(640, 480);
-  colorMode( HSB, 360, 100, 100);
+  colorMode(HSB, 360, 100, 100);
   textFont(fuente);
-  pag =0;
+  pag = 0;
   color1 = color(94, 100, 78);
   color2 = color(65, 78, 93);
   animacion = 0;
-
+  console.log(dialogos);
 }
 
 function draw() {
+  botonSig = (mouseX >= 600 && mouseX < 630 && mouseY >= 440 && mouseY < 460);
   botonInicio = (mouseX >= 250 && mouseX < 410 && mouseY >= 302 && mouseY < 332);
   botonCreditos = (mouseX >= 270 && mouseX < 380 && mouseY >= 352 && mouseY < 382);
-  console.log(mouseY, mouseX);
-  if(pag==0){
+
+  if (pag == 0) {
+    // PANTALLA DE INICIO
+    botonSig = false;
     background(220, 15, 50);
     image(fondo[pag], 0, 0);
-  
+
     fill(color1);
     textSize(70);
-    text("MISTERIOS",135, 122);
+    text("MISTERIOS", 135, 122);
     textSize(60);
     text("EN LA", 250, 175);
     textSize(70);
     text("MANSION", 210, 232);
     textSize(30);
 
-    if(botonInicio){
-        fill(color2);
-    }else{
-        fill(color1);
-    }
+    if (botonInicio) fill(color2); else fill(color1);
     circle(mouseX, mouseY, 10);
     stroke(0);
     strokeWeight(6);
     text("INICIAR JUEGO", 250, 330);
-    if(botonCreditos){
-        fill(color2);
-    }else{
-        fill(color1);
-    }
+
+    if (botonCreditos) fill(color2); else fill(color1);
     text("CREDITOS", 275, 380);
 
-
-  } else if (pag==1){
+  } else if (pag == 1) {
+    //CINEMATICA 1
     background(100, 12, 80);
     image(fondo[pag], 0, -140);
+    fill(color1);
     text(dialogos[0], 50, 380);
-
-    animacion++
-    text(int(animacion / 60), 600, 400);
-    if (animacion > 150) {
-      pag = 2;
-      animacion = 0;
-    }
-
-  } else if (pag == 2){
-    background(20);
-    image(fondo[pag], 0, 0);
-    text(dialogos[1], 50, 380);
-
     
-    animacion++
-    text(int(animacion / 60), 600, 400);
-    if (animacion > 150) {
-      pag = 3;
-      animacion = 0;
+    if (botonSig){
+      fill(color2); 
+    }else {
+      fill(color1);
     }
+      text("->", 600, 460);
 
-  } else if (pag == 3){
+  } else if (pag == 2) {
+    //CINEMATICA 2
+    botonSig = true;
     background(20);
     image(fondo[pag], 0, 0);
+    fill(color1);
+    text(dialogos[1], 50, 380);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+
+  } else if (pag == 3) {
+    //CINEMATICA 3
+    background(20);
+    image(fondo[pag], 0, 0);
+    fill(color1);
     text(dialogos[2], 10, 430, 300);
     text(dialogos[3], 350, 350, 300);
     
-    animacion++
-    text(int(animacion / 60), 600, 400);
-    if (animacion > 150) {
-      pag = 4;
-      animacion = 0;
-    }
-  }else if (pag == 4){
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+
+  } else if (pag == 4) {
+    //CINEMATICA 4
     background(20);
     image(fondo[pag], 0, 0);
-    text(dialogos[4],150, 400);
+    fill(color1);
+    text(dialogos[4], 150, 400);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
 
-    animacion++
-    text(int(animacion / 60), 600, 400);
-    if (animacion > 150) {
-      pag = 5;
-      animacion = 0;
-    }
-  }else if (pag == 5){
+  } else if (pag == 5) {
+    //CINEMATICA 5
     background(20);
     image(fondo[pag], 0, 0);
-    text(dialogos[5],180, 400);
+    fill(color1);
+    text(dialogos[5], 180, 400);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
 
-    animacion++
-    text(int(animacion / 60), 600, 400);
-    if (animacion > 150) {
-      pag = 6;
-      animacion = 0;
-    }
-  }else if (pag == 6){
+  } else if (pag == 6) {
+    //CINEMATICA 6
     background(20);
     image(fondo[pag], 0, 0);
-    text(dialogos[6],20, 400, 300);
-    text(dialogos[7],400, 400);
+    fill(color1);
+    text(dialogos[6], 20, 400, 300);
+    text(dialogos[7], 400, 400);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
 
-    animacion++
-    text(int(animacion / 60), 600, 400);
-    if (animacion > 150) {
-      pag = 7;
-      animacion = 0;
-    }
-  }else if (pag == 7){
+  } else if (pag == 7) {
+    //CINEMATICA 7
     background(20);
     image(fondo[pag], 0, -100);
-    text(dialogos[8],30, 400, 300);
-    text(dialogos[9],370, 300, 230);
+    fill(color1);
+    text(dialogos[8], 30, 400, 300);
+    text(dialogos[9], 370, 300, 230);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  } else if (pag == 8) {
+    // DECISIÓN QUEDARSE / IRSE
+    botonSig = false;
+    background(20);
+    image(fondo[pag], 0, 0);
 
-    animacion++
-    text(int(animacion / 60), 600, 400);
-    if (animacion > 150) {
-      animacion = 0;
-    }
+    fill(color1);
+    textSize(35);
+    text(dialogos[10], 200, 100);
+
+    botonQuedarse = (mouseX >= 120 && mouseX < 230 && mouseY >= 300 && mouseY < 330);
+    botonIrse = (mouseX >= 390 && mouseX < 450 && mouseY >= 300 && mouseY < 330);
+
+    if (botonQuedarse) fill(color2); else fill(color1);
+    stroke(0);
+    strokeWeight(6);
+    textSize(30);
+    text(dialogos[11], 120, 330);
+
+    if (botonIrse) fill(color2); else fill(color1);
+    text(dialogos[12], 400, 330);
+  }
+  // QUEDARSE
+  // BUSCAR OTRA ENTRADA
+  // === Nueva pantalla de decisión con 3 botones ===
+  else if (pag == 9) {
+    background(20);
+    image(fondo[pag], 0, 0);
+
+    fill(color1);
+    textSize(35);
+    text(dialogos[13], 150, 100);
+
+    boton1 = (mouseX >= 50 && mouseX < 300 && mouseY >= 300 && mouseY < 340);
+    boton2 = (mouseX >= 320 && mouseX < 580 && mouseY >= 300 && mouseY < 340);
+    boton3 = (mouseX >= 50 && mouseX < 580 && mouseY >= 360 && mouseY < 400);
+
+    if (boton1) fill(color2); else fill(color1);
+    stroke(0);
+    strokeWeight(6);
+    textSize(30);
+    text(dialogos[14], 60, 330);
+
+    if (boton2) fill(color2); else fill(color1);
+    text(dialogos[15], 330, 330);
+
+    if (boton3) fill(color2); else fill(color1);
+    text(dialogos[16], 60, 385);
+    
+  }
+  // BUSCANDO
+  // === Pantalla llaves-tumba ===
+  else if (pag == 10) {
+    botonSig = true;
+    background(60);
+    image(gif[0], 0, 60);
+   
+
+    fill(color1);
+    textSize(30);
+    text(dialogos[17], 30, 400);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  // SE ENCUENTRA LA PALA
+  else if (pag == 11) {
+    background(20);
+    image(fondo[pag-1], 0, 0);
+
+    fill(color1);
+    text(dialogos[18], 150, 360);
+    text(dialogos[19], 150, 400);
+    text(dialogos[20], 130, 440);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+}
+else if (pag == 12) {
+  // REVISANDO TUMBAS
+    background(20);
+    image(fondo[pag-1], 0, 0);
+
+    textSize(30);
+    fill(40, 1);
+    rect(40, 210, 550, 100);
+    fill(color1);
+    text(dialogos[21], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 13) {
+    // VOLVER A LA ENTRADA 1/2
+    background(20);
+    image(fondo[pag-1], 0, 0);
+
+    fill(color1);
+    text(dialogos[22], 55, 440);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 14) {
+    // VOLVER A LA ENTRADA 2/2
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 15) {
+    // INTERIOR MANSION - DECISION
+    botonSig = false;
+    background(20);
+    image(fondo[pag-2], 0, 0);
+    boton1 = (mouseX >= 50 && mouseX < 212 && mouseY >= 220 && mouseY < 250);
+    boton2 = (mouseX >= 320 && mouseX < 580 && mouseY >= 220 && mouseY < 250);
+
+    fill(color1);
+    text(dialogos[24], 220, 145, 570);
+    if (boton1) fill(color2); else fill(color1);
+    text(dialogos[25], 55, 245, 570);
+    if (boton2) fill(color2); else fill(color1);
+    text(dialogos[26], 455, 245, 570);
+    
+  }
+  else if (pag == 16) {
+    // ESTUDIO
+    background(20);
+    botonSig = true;
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 30, 580, 80);
+    fill(color1);
+    text(dialogos[27], 55, 65, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 17) {
+    // LIBRERO
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 100, 570, 95);
+    fill(color1);
+    text(dialogos[28], 55, 135, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 18) {
+    // PASADIZO
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 540, 155);
+    fill(color1);
+    text(dialogos[29], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 19) {
+    // LOS ENCUENTRA
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(120, 310, 400, 50);
+    fill(color1);
+    text(dialogos[30], 130, 345, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 20) {
+    // INTENTO DE SOLTARLOS
+    boton1 = (mouseX >= 40 && mouseX < 260 && mouseY >= 215 && mouseY < 290);
+    boton2 = (mouseX >= 385 && mouseX < 610 && mouseY >= 215 && mouseY < 250);
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(color1);
+    text(dialogos[31], 220, 145, 570);
+    if (boton1) fill(color2); else fill(color1);
+    text(dialogos[32], 45, 245, 240);
+    if (boton2) fill(color2); else fill(color1);
+    text(dialogos[33], 390, 245, 240);
+    
+  }
+  else if (pag == 21) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 22) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 23) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 24) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 25) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 26) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 27) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 28) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 29) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 30) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
+  }
+  else if (pag == 31) {
+    background(20);
+    image(fondo[pag-2], 0, 0);
+
+    fill(40, 1);
+    rect(40, 210, 570, 100);
+    fill(color1);
+    text(dialogos[23], 55, 245, 570);
+    
+    if (botonSig) fill(color2); else fill(color1);
+    text("->", 600, 460);
   }
 }
+function mouseClicked() {
+  if (pag == 0) {
+    if (botonInicio) pag = 1;
+    if (botonCreditos) pag = 17;
+  }
 
-function mouseClicked(){
-  if(pag == 0){
-    if(botonInicio){
-      pag=1;
+  if (pag == 8) {
+    if (botonQuedarse) pag = 9;
+    if (botonIrse) pag = 0;
+  }
+
+  if (pag == 9) {
+    if (boton1) pag = 10;
+    if (boton2) {
+      // acción pendiente
     }
-    if(botonCreditos){
-      pag = 7;
+    if (boton3) {
+      // acción pendiente
     }
   }
-  if(fondo == fondo.length){
-    pag=0;
+  if (pag == 15){
+    if(boton1){
+
+    }
+    if(boton2){
+      pag = 16;
+    }
+  }
+  if (pag == 20){
+    if(boton1){
+      pag = 21;
+    }
+    if(boton2){
+    }
+  }
+  if(botonSig){
+    pag++;
   }
 }
